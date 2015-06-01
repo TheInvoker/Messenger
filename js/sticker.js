4dfd60f17c6dc045decc2cc075ab0c802fb8f85d
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-05-31 21:12:03 -0400

    updates

--- js/sticker.js
+++ js/sticker.js
@@ -1,82 +1,73 @@
 var sticker = function(obj, base_scale) {
+	
 	var interval = -1;
 	var svgDoc = obj.contentDocument; 
-	var cm = svgDoc.getElementById("mainGroup")
+	var cm = svgDoc.getElementById("main")
 	var w = parseFloat($(cm).parent().attr("width"));
 	var h = parseFloat($(cm).parent().attr("height"));
+	
+	var body = new component(svgDoc.getElementById("body"), w, h, base_scale);
+	var head = new component(svgDoc.getElementById("head"), w, h, base_scale);
+	var rightArm = new component(svgDoc.getElementById("right_arm"), w, h, base_scale);
+	var leftArm = new component(svgDoc.getElementById("left_arm"), w, h, base_scale);
+	var rightLeg = new component(svgDoc.getElementById("right_leg"), w, h, base_scale);
+	var leftLeg = new component(svgDoc.getElementById("left_leg"), w, h, base_scale);
 	
 	var componants = $(cm).find('path,polygon,polyline').map(function(i, c) {
 		return new component(c, w, h, base_scale);
 	});
 	
-	var rightArm = [
-		new component(svgDoc.getElementById("right_arm"), w, h, base_scale),
-		new component(svgDoc.getElementById("right_wrist"), w, h, base_scale), 
-		new component(svgDoc.getElementById("right_slieve"), w, h, base_scale), 
-		new component(svgDoc.getElementById("right_hand"), w, h, base_scale)
-	];
-	var leftArm = [
-		new component(svgDoc.getElementById("left_arm"), w, h, base_scale),
-		new component(svgDoc.getElementById("left_wrist"), w, h, base_scale), 
-		new component(svgDoc.getElementById("left_slieve"), w, h, base_scale), 
-		new component(svgDoc.getElementById("left_hand"), w, h, base_scale)
-	];
-	var rightLeg = [
-		new component(svgDoc.getElementById("right_thigh"), w, h, base_scale),
-		new component(svgDoc.getElementById("right_limb"), w, h, base_scale), 
-		new component(svgDoc.getElementById("right_foot"), w, h, base_scale)
-	];
-	var leftLeg = [
-		new component(svgDoc.getElementById("left_thigh"), w, h, base_scale),
-		new component(svgDoc.getElementById("left_limb"), w, h, base_scale), 
-		new component(svgDoc.getElementById("left_foot"), w, h, base_scale)
-	];
-	var body = [
-		new component(svgDoc.getElementById("body"), w, h, base_scale)
-	];
-	var neck = [
-		new component(svgDoc.getElementById("neck"), w, h, base_scale)
-	];
-	var head = [
-		new component(svgDoc.getElementById("left_ear"), w, h, base_scale),
-		new component(svgDoc.getElementById("right_ear"), w, h, base_scale),
-		new component(svgDoc.getElementById("face"), w, h, base_scale),
-		new component(svgDoc.getElementById("left_eye"), w, h, base_scale),
-		new component(svgDoc.getElementById("right_eye"), w, h, base_scale),
-		new component(svgDoc.getElementById("mouth"), w, h, base_scale),
-		new component(svgDoc.getElementById("hair"), w, h, base_scale)
-	];
-
-	
-	this.wobble = function() {
-		clearInterval(interval);
+	var reset = function() {
+		transform(cm, w, h, 0, base_scale, 0, 0, 0, 0, 0, 0);
 		
+		$(cm).find('g').each(function(i, x) {
+			transform(x, w, h, 0, 1, 0, 0, 0, 0, 0, 0);
+		});
+
+		var i, l=componants.length;
+		for(i=0; i<l; i+=1) {
+			transform(componants[i].getComponent(), w, h, 0, 1, 0, 0, 0, 0, 0, 0);
+		}
+	};
+
+	this.wobble = function() {
 		var step = 0;
 		var sinscale = 0.1;
 		interval = setInterval(function(){ 
 			transform(cm, w, h, 0, base_scale, Math.sin(step)*sinscale, -Math.sin(step)*sinscale, 0, 0, 0, 0);
 			step += 0.05;
 		}, 1);
+		
+		setTimeout(function() {
+			reset();
+			clearInterval(interval);
+		}, 1000);
 	};
 	
 	this.jump = function() {
-		clearInterval(interval);
-		
 		var step = 0;
 		interval = setInterval(function(){ 
-			transform(cm, w, h, 0, base_scale, 0, 0, 0, 0, 0, 10*(Math.sin(step)+1));
+			transform(cm, w, h, 0, base_scale, 0, 0, 0, 0, 0, -10*(Math.sin(step)+1));
 			step += 0.1;
 		}, 1);
+		
+		setTimeout(function() {
+			reset();
+			clearInterval(interval);
+		}, 200);
 	};
 	
 	this.twirl = function() {
-		clearInterval(interval);
-		
 		var step = 0;
 		interval = setInterval(function() { 
 			transform(cm, w, h, step, base_scale, 0, 0, 0, 0, 0, 0);
 			step += 1;
 		}, 1);
+		
+		setTimeout(function() {
+			reset();
+			clearInterval(interval);
+		}, 1850);
 	};
 	
 	this.explode = function() {
@@ -85,46 +76,44 @@ var sticker = function(obj, base_scale) {
 			var c = componants[i];
 			c.fly();
 		}
+		
+		setTimeout(function() {
+			reset();
+		}, 1000);
 	};
 	
 	this.kick = function() {
-		clearInterval(interval);
-		
-		var step = 0;
+		var step = 0, rotation_angle = 60;
 		interval = setInterval(function() {
 			transform(cm, w, h, 0, base_scale, 0, 0, 0, 0, 0, 0);
-		
-			var i, l=rightLeg.length;
-			for(i=0; i<l; i+=1) {
-				var c = rightLeg[i];
-				transform(c.getComponent(), w, h, (Math.sin(step)+1)/2 * -60, 1, 0, 0, 220 - w/2, 280 - h/2, 0, 0);
-			}
-			step += 0.1;
+			transform(rightLeg.getComponent(), w, h, (Math.sin(step)+1)/2 * -rotation_angle, 1, 0, 0, rightLeg.getJX() - w/2, rightLeg.getJY() - h/2, 0, 0);
 			
-			if (step > Math.PI*1.5) {
-				clearInterval(interval);
-			}
+			step += 0.1;
 		}, 10);
+		
+		setTimeout(function() {
+			reset();
+			clearInterval(interval);
+		}, 500);
 	};
 	
-	this.walk = function() {
-		clearInterval(interval);
-		
-		var step = 0;
+	this.dance = function() {
+		var step = 0, rotation_angle = 40;
 		interval = setInterval(function() {
-			transform(cm, w, h, 0, base_scale, 0, 0, 0, 0, 0, 0);
-		
-			var i, l=rightLeg.length;
-			for(i=0; i<l; i+=1) {
-				var c = rightLeg[i];
-				transform(c.getComponent(), w, h, (Math.cos(step))/2 * -40, 1, 0, 0, 220 - w/2, 280 - h/2, 0, 0);
-			}
-			var i, l=leftLeg.length;
-			for(i=0; i<l; i+=1) {
-				var c = leftLeg[i];
-				transform(c.getComponent(), w, h, (Math.sin(step))/2 * 40, 1, 0, 0, 179 - w/2, 280 - h/2, 0, 0);
-			}
+			transform(head.getComponent(), w, h, (Math.cos(step))/2 * -rotation_angle, 1, 0, 0, head.getJX() - w/2, head.getJY() - h/2, 0, 0);
+			transform(body.getComponent(), w, h, (Math.sin(step))/2 * -rotation_angle/4, 1, 0, 0, body.getJX() - w/2, body.getJY() - h/2, 0, 0);
+			transform(rightArm.getComponent(), w, h, (Math.cos(step))/2 * -rotation_angle-90, 1, 0, 0, rightArm.getJX() - w/2, rightArm.getJY() - h/2, 0, 0);
+			transform(leftArm.getComponent(), w, h, (Math.sin(step))/2 * -rotation_angle+90, 1, 0, 0, leftArm.getJX() - w/2, leftArm.getJY() - h/2, 0, 0);
+			transform(rightLeg.getComponent(), w, h, (Math.cos(step))/2 * -rotation_angle, 1, 0, 0, rightLeg.getJX() - w/2, rightLeg.getJY() - h/2, 0, 0);
+			transform(leftLeg.getComponent(), w, h, (Math.sin(step))/2 * rotation_angle, 1, 0, 0, leftLeg.getJX() - w/2, leftLeg.getJY() - h/2, 0, 0);
 			step += 0.1;
 		}, 10);
+		
+		setTimeout(function() {
+			reset();
+			clearInterval(interval);
+		}, 1000);
 	};
+	
+	reset();
 };
