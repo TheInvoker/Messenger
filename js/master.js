26b97c89ec05bf9a51f1421601e9efc2c77a47be
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-06-14 19:23:29 -0400

    updated mapping files

--- js/master.js
+++ js/master.js
@@ -1,18 +1,27 @@
-var stickerMasterList = [];
-var masterMapping = [
+// this defines all the svg files we need
+var masterStickerList = [
 	{
-		folder : 'steve'
+		name : 'steve',
+		sklcls : 'mammal',
+		stillSvg : 'images/steve/steve.svg',
+		TSvg : 'images/steve/steve.svg',
+		right_arm_group : [227, 208],
+		left_arm_group : [170, 208],
+		torso_group : [200, 237],
+		head_group : [200, 193],
+		right_leg_group : [220, 280],
+		left_leg_group : [180, 280]
 	},
 	{
-		folder : 'yeti'
+		name : 'yeti',
+		sklcls : 'mammal',
+		stillSvg : 'images/yeti/yeti-01.svg',
+		TSvg : 'images/yeti/yeti-01.svg',
+		right_arm_group : [227, 208],
+		left_arm_group : [170, 208],
+		torso_group : [200, 237],
+		head_group : [200, 193],
+		right_leg_group : [220, 280],
+		left_leg_group : [180, 280]
 	}
 ];
-
-$(document).ready(function() {
-	var lst = [];
-	for (var i=0; i<masterMapping.length; i+=1) {
-		var obj = masterMapping[i];
-		lst.push(sprintf("<script src='images/%s/mapping.js'></script>", obj.folder));
-	}	
-	$("body").append(lst.join(""));
-});
